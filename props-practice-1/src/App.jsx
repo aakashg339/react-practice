1f528d7bfb709fab5b4d834ffab2b0a3259b4e84
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-01-08 10:26:16 +0530

    refactor(props-practice-1): Using spread operator instead of passing indivitual props.

--- props-practice-1/src/App.jsx
+++ props-practice-1/src/App.jsx
@@ -6,18 +6,26 @@ function App() {
     alert(`You clicked on: ${hobby}`);
   };
 
+  const aliceProfile ={
+    name: "Alice",
+    age: 23,
+    isMember: true, 
+    hobbies: ['Reading', 'Cooking'],
+    onHobbyClick: handleHobbyClick
+  };
+
+  const johnProfile = {
+    name: "John", 
+    age: 42, 
+    isMember: false, 
+    hobbies: ['Swimmming', 'Hiking'],
+    onHobbyClick: handleHobbyClick
+  };
+
   return (
     <div className='app-container'>
-      <ProfileCard name="Alice" 
-                  age="23" 
-                  isMember={true} 
-                  hobbies={['Reading', 'Cooking']}
-                  onHobbyClick={handleHobbyClick} />
-      <ProfileCard name="John" 
-                  age="42" 
-                  isMember={false} 
-                  hobbies={['Swimmming', 'Hiking']}
-                  onHobbyClick={handleHobbyClick} />
+      <ProfileCard {...aliceProfile} />
+      <ProfileCard {...johnProfile} />
     </div>
   )
 }
