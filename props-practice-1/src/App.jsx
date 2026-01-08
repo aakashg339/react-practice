b8585bef00a824b49762f51b503bdc792f50a1b0
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-01-08 08:14:28 +0530

    feat(props-practice-1): Added array props to be passed to component.

--- props-practice-1/src/App.jsx
+++ props-practice-1/src/App.jsx
@@ -6,8 +6,8 @@ function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <ProfileCard name="Alice" age="23" isMember={true} />
-      <ProfileCard name="John" age="42" isMember={false} />
+      <ProfileCard name="Alice" age="23" isMember={true} hobbies={['Reading', 'Cooking']} />
+      <ProfileCard name="John" age="42" isMember={false} hobbies={['Swimmming', 'Hiking']} />
     </div>
   )
 }
--- props-practice-1/src/ProfileCard.jsx
+++ props-practice-1/src/ProfileCard.jsx
@@ -1,9 +1,14 @@
-function ProfileCard({name, age, isMember}) {
+function ProfileCard({name, age, isMember, hobbies}) {
     return (
         <div>
             <h3>Name: {name}</h3>
             <h3>Age: {age}</h3>
             <h3>Status: {isMember ? "Active Member" : "Guest"}</h3>
+            <ul>
+                {hobbies.map((hobby, index) => {
+                    return <li key={index}>{hobby}</li>;
+                })}
+            </ul>
         </div>
     );
 }
