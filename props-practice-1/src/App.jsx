4ae7bf296f495948932a8fd5ddaedddde82d469c
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-01-08 10:17:41 +0530

    feat(props-practice-1): Passed callback as props to child component

--- props-practice-1/src/App.jsx
+++ props-practice-1/src/App.jsx
@@ -2,11 +2,22 @@ import './App.css'
 import ProfileCard from './ProfileCard'
 
 function App() {
+  const handleHobbyClick = (hobby) => {
+    alert(`You clicked on: ${hobby}`);
+  };
 
   return (
     <div className='app-container'>
-      <ProfileCard name="Alice" age="23" isMember={true} hobbies={['Reading', 'Cooking']} />
-      <ProfileCard name="John" age="42" isMember={false} hobbies={['Swimmming', 'Hiking']} />
+      <ProfileCard name="Alice" 
+                  age="23" 
+                  isMember={true} 
+                  hobbies={['Reading', 'Cooking']}
+                  onHobbyClick={handleHobbyClick} />
+      <ProfileCard name="John" 
+                  age="42" 
+                  isMember={false} 
+                  hobbies={['Swimmming', 'Hiking']}
+                  onHobbyClick={handleHobbyClick} />
     </div>
   )
 }
