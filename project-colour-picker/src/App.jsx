3d7ec428a1e031e5e59054e78a4cc75eac1121b5
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-01-09 11:04:49 +0530

    feat(project-colour-picker): Added hook and div.
    Added useState hook to keep record of current background colour.
    Added logic to display the colour boxes div.

--- project-colour-picker/src/App.jsx
+++ project-colour-picker/src/App.jsx
@@ -2,11 +2,23 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [backgroundColour, setBackgroundColour] = useState('#ffffff');
+  const colours = ['#ff0000'];
 
   return (
     <div>
+      <h1>Colour Picker</h1>
+      <div className='colour-palette'>
+        {colours.map((colour, index) => (
+          <div
+            key={index}
+            className='colour-box'
+            style={{backgroundColor: colour}}
+            onClick={() => {}}>
 
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
