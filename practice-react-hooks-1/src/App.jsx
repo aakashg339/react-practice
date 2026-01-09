cba307699828ce135278787dd5d9c959958e4bbe
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-01-09 06:13:20 +0530

    feat(practice-react-hooks-1): Added step state to decide step for counter

--- practice-react-hooks-1/src/App.jsx
+++ practice-react-hooks-1/src/App.jsx
@@ -3,15 +3,20 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
 
-  const incrementCount = () => setCount(count+1);
-  const decrementCount = () => setCount(count-1);
+  const incrementCount = () => setCount(count+step);
+  const decrementCount = () => setCount(count-step);
 
   return (
     <div className='app-container'>
       <h1>Counter Value: {count}</h1>
+      <input type='number'
+              value={step}
+              onChange={(e) => setStep(parseInt(e.target.value))} />
       <button onClick={incrementCount}>Increment</button>
       <button onClick={decrementCount}>Decrement</button>
+
     </div>
   )
 }
