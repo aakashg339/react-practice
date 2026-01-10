3e5eca233123434766fecc270a52e127b858e76a
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-01-10 16:31:34 +0530

    feat(practice-react-useRef-withHtml): Added simple example to use useRef with html element

--- practice-react-useRef-withHtml/src/App.jsx
+++ practice-react-useRef-withHtml/src/App.jsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const inputRef1 = useRef(null);
+  const inputRef2 = useRef(null);
+
+  useEffect(() => {
+    console.log('Component Rerendered');
+  });
+
+  const focusInput1 = () => {
+    inputRef1.current.focus();
+    inputRef1.current.style.background = 'yellow';
+  };
+
+  const focusInput2 = () => {
+    inputRef2.current.focus();
+    inputRef2.current.style.background = 'yellow';
+  };
+
+  const resetInput = () => {
+    inputRef1.current.style.background = 'white';
+    inputRef2.current.style.background = 'white';
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Learn React</h1>
+      <input ref={inputRef1} type='text' placeholder='Focus me'></input>
+      <button onClick={focusInput1}>Focus and Highlight</button>
+
+      <input ref={inputRef2} type='text' placeholder='Focus me'></input>
+      <button onClick={focusInput2}>Focus and Highlight</button>
+
+      <button onClick={resetInput}>Reset</button>
+    </div>
   )
 }
 
