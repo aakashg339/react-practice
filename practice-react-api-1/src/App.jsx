ab0cabdca02bf987767607687a3fd1c0cfda47b7
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-01-11 10:51:14 +0530

    feat(practice-react-api-1): Changes for loading and error messages.
    Created state and return html accordingly for loading and error scenarios.

--- practice-react-api-1/src/App.jsx
+++ practice-react-api-1/src/App.jsx
@@ -3,12 +3,33 @@ import './App.css'
 
 function App() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    setLoading(true);
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
-      .then(json => setData(json));
+      .then(json => {
+        setData(json);
+        setLoading(false);
+
+        throw new Error('Something went wrong');
+      })
+      .catch((e) => {
+        console.error(`Error fetching data: ${e}`);
+        setError('Unable to fetch the data');
+        setLoading(false);
+      })
   }, []);
+
+  if(loading) {
+    return <p>Loading......</p>
+  }
+
+  if(error) {
+    return <p>{error}</p>
+  }
 
   return (
     <div>
