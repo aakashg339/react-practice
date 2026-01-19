09d03b9da74c3f515af5cfcd747199ff08d652d9
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-01-19 08:30:07 +0530

    feat(practice-react-api-2): Added axios POST request
    Made POST request using axios.

--- practice-react-api-2/src/App.jsx
+++ practice-react-api-2/src/App.jsx
@@ -1,14 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState();
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newPost = {
+      title: 'foo',
+      body: 'bar',
+      userId: 1
+    };
+
+    axios.post('https://jsonplaceholder.typicode.com/posts', newPost)
+      .then((response) => {
+        console.log('New Post Added: ', response.data);
+        setData([response.data]);
+      });
+  };
 
   return (
     <div>
       <h1>API's in React</h1>
+      <form onSubmit={handleSubmit}>
+        <button type='submit'>Add Post</button>
+      </form>
     </div>
   )
 }
