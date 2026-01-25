99a6f2447d0ce313f1a821de4501efb8398bda56
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-01-25 17:08:25 +0530

    feat: Added form for name and email

--- practice-react-forms-1/src/App.jsx
+++ practice-react-forms-1/src/App.jsx
@@ -1,10 +1,40 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: ''
+  });
+
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    console.log('Form Data Submitted ', formData);
+  };
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
     <div>
       <h1>Forms in React</h1>
+      <form onSubmit={handleSumbit}>
+        <label>
+          Name:
+          <input type='text' name='name' value={formData.name} onChange={handleChange}></input>
+        </label>
+
+        <label>
+          Email:
+          <input type='email' name='email' value={formData.email} onChange={handleChange}></input>
+        </label>
+
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   )
 }
