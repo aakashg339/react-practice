7176d10999050e01fdad99cd3eb06b1ea0252fdc
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-01-25 17:38:45 +0530

    feat: Added validations for form

--- practice-react-forms-1/src/App.jsx
+++ practice-react-forms-1/src/App.jsx
@@ -7,16 +7,39 @@ function App() {
     email: ''
   });
 
+  const [errors, setErrors] = useState({});
+
+  const validate = () => {
+    const newErrors = {};
+    if(!formData.name.trim()) {
+      newErrors.name = 'Name is required';
+    }
+    return newErrors;
+  };
+
   const handleSumbit = (e) => {
     e.preventDefault();
-    console.log('Form Data Submitted ', formData);
+    const validationErrors = validate();
+    if(Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+    } else {
+      console.log('Form Data Submitted ', formData);
+    }
   };
 
   const handleChange = (e) => {
+    const {name, value} = e.target;
+    
     setFormData({
       ...formData,
       [e.target.name]: e.target.value,
     });
+
+    if(errors[name]) {
+      const newErrors = {...errors};
+      delete newErrors[name];
+      setErrors(newErrors);
+    }
   };
 
   return (
@@ -25,12 +48,16 @@ function App() {
       <form onSubmit={handleSumbit}>
         <label>
           Name:
-          <input type='text' name='name' value={formData.name} onChange={handleChange}></input>
+          <input type='text' name='name' value={formData.name} onChange={handleChange}>
+          </input>
+          {errors.name && <span style={{color:'red'}}>{errors.name}</span>}
+          <br/>
         </label>
 
         <label>
           Email:
-          <input type='email' name='email' value={formData.email} onChange={handleChange}></input>
+          <input type='email' name='email' value={formData.email} onChange={handleChange}>
+          </input>
         </label>
 
         <button type='submit'>Submit</button>
