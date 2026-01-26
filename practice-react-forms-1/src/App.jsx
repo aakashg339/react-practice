301308483c11f660c837ee345ef90974eb5803e2
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-01-26 11:00:18 +0530

    feat(practice-react-forms-1): Added more validations and triggring validations on every change

--- practice-react-forms-1/src/App.jsx
+++ practice-react-forms-1/src/App.jsx
@@ -3,12 +3,20 @@ import './App.css'
 import {useForm} from 'react-hook-form'
 
 function App() {
-  const {register, handleSubmit, reset, watch, formState: {errors}} = useForm();
+  const {register, handleSubmit, reset, watch, formState: {errors}} = useForm({
+    mode: 'onChange'
+  });
   
   const onSubmit = (data) => {
     console.log("Form Data: ", data);
     reset();
   }
+
+  const existingUsernames = ['admin', 'user123', 'john'];
+  const checkIfUsernameExist = async (username) => {
+    await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+    return existingUsernames.includes(username);
+  };
 
   // const validateName = (value) => {
   //   if(value !== 'admin') {
@@ -46,17 +54,57 @@ function App() {
                 validate: {
                   notAdmin: (value) => value !== "admin" || "Admin is not allowed",
                   isNotNumber: (value) => isNaN(value) || "Name cannot be number",
+                  checkUsername: async (value) => {
+                    const exist = await checkIfUsernameExist(value);
+                    return !exist || 'Username already taken';
+                  }
                 }
               }
           )} />
         </label>
         {errors.name && <p>{errors.name.message}</p>}
+        <br />
 
         <label>
           Email:
           <input {...register('email', {required: 'Email is required', pattern: {value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, message: 'Enter a valid email'}})} />
         </label>
         {errors.email && <p>{errors.email.message}</p>}
+        <br />
+
+        <label>
+          Password:
+          <input type='password' {...register('password', 
+            {
+              required: 'Name is required', 
+              minLength: 
+                {
+                  value: 2, 
+                  message:'Name should be atleast 2 character'
+                },
+            }
+          )}>
+          </input>
+        </label>
+        <br />
+
+        <label>
+          Confirm Password:
+          <input type='password' {...register('confirmPassword', 
+            {
+              required: 'Name is required', 
+              minLength: 
+                {
+                  value: 2, 
+                  message:'Name should be atleast 2 character'
+                },
+              validate: value => value === watch('password') || 'Passwords do not match'
+            }
+          )}>
+          </input>
+        </label>
+        {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
+        <br />
 
         <button type='submit'>Submit</button>
         <button type='button' onClick={() => reset()}>Reset</button>
