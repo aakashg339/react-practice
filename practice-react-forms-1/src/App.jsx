055f8b245c2c2f618da1580c57c45a1ffb267de1
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-01-26 08:04:38 +0530

    feat(practice-reactt-forms-1): Added validations to form

--- practice-react-forms-1/src/App.jsx
+++ practice-react-forms-1/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import {useForm} from 'react-hook-form'
 
 function App() {
-  const {register, handleSubmit, watch} = useForm();
+  const {register, handleSubmit, watch, formState: {errors}} = useForm();
   const onSubmit = (data) => console.log(data);
 
   // console.log(watch('name'));
@@ -25,13 +25,20 @@ function App() {
       <form onSubmit={handleSubmit(onSubmit)}>
         <label>
           Name:
-          <input {...register('name')} />
+          {/* <input {...register('name', {required: true, minLength: 2})} /> */}
+          <input {...register('name', 
+              {required: 'Name is required', 
+                minLength: {value: 2, 
+                  message:'Name should be atleast 2 character'
+          }})} />
         </label>
+        {errors.name && <p>{errors.name.message}</p>}
 
         <label>
           Email:
-          <input {...register('email')} />
+          <input {...register('email', {required: 'Email is required', pattern: {value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, message: 'Enter a valid email'}})} />
         </label>
+        {errors.email && <p>{errors.email.message}</p>}
 
         <button type='submit'>Submit</button>
       </form>
