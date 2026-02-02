7be0f44c7411f30c0d92edb5f806b3d8cab70d3c
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-02-02 10:58:10 +0530

    feat(practice-react-routing-1): Added links to different routes

--- practice-react-routing-1/src/App.jsx
+++ practice-react-routing-1/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import Home from './Home'
 import About from './About';
 import Contact from './Contact';
@@ -8,6 +8,13 @@ function App() {
 
   return (
     <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
