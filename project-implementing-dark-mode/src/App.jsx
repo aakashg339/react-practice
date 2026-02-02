d5066b7647c212691e4816c0a9f4b0990168881a
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-02-02 08:31:16 +0530

    feat(project-implementing-dark-mode): Added example of dark mode

--- project-implementing-dark-mode/src/App.jsx
+++ project-implementing-dark-mode/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import DarkModeToggle from './DarkModeToggle'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <div className='p-4 bg-whie min-h-screen dark:bg-gray-900 dark:text-white'>
+      <DarkModeToggle />
+      <h1 className='text-2xl'>Welcome to Dark Mode App</h1>
+      <p>This is an example of implementing dark mode using Tailwind CSS.</p>
+    </div>
   )
 }
 
