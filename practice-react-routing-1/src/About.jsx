1fdaf4142f8056f75272041ad3a37ae14b775c04
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-02-03 05:51:25 +0530

    feat(practice-react-routing-1): Added css to pages

--- practice-react-routing-1/src/About.jsx
+++ practice-react-routing-1/src/About.jsx
@@ -1,5 +1,14 @@
 function About() {
-    return <h2>About</h2>;
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+            <h2 
+                className="text-3xl font-bold text-blue-600">
+                    About Page</h2>
+            <p 
+                className="text-gray-700 mt-4 text-lg">
+                Feel free to reach out to us through our contact form or email. We would love to hear from you.</p>
+        </div>
+    )
 }
 
 export default About;
--- practice-react-routing-1/src/App.jsx
+++ practice-react-routing-1/src/App.jsx
@@ -8,18 +8,28 @@ function App() {
 
   return (
     <Router>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-      </Routes>
+      <div className='min-h-screen bg-gray-100'>
+        <nav className='bg-blue-600 p-4'>
+          <ul className='flex justify-center space-x-6'>
+            <li><Link 
+                className='text-white hover:text-yellow-300 font-medium transition duration-300'
+                to="/">Home</Link></li>
+            <li><Link 
+                className='text-white hover:text-yellow-300 font-medium transition duration-300'
+                to="/about">About</Link></li>
+            <li><Link 
+                className='text-white hover:text-yellow-300 font-medium transition duration-300'
+                to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+        <div className='container mx-auto p-5'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/contact' element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   )
 }
