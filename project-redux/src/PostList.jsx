6a97aefee69b8acdd4a92d9e36b34db02a913bbd
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-02-04 08:03:30 +0530

    feat(project-redux): Created store, actions and reducer for posts

---
+++ project-redux/src/PostList.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPosts } from "./store/actions/postActions";
+
+const PostList = () => {
+    const dispatch = useDispatch();
+    const { posts, loading, error } = useSelector((state) => state.posts)
+
+    useEffect(() => {
+        dispatch(fetchPosts());
+    }, [dispatch]);
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default PostList;
